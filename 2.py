eef2f4cdbf1991a267d3980e7f56f02abe73686f
Raghav Mishra <raghav051006@gmail.com>
2025-11-17 09:26:09 +0530

    update 2.py

--- 2.py
+++ 2.py
@@ -9,22 +9,30 @@ class Solution(object):
         :type l2: Optional[ListNode]
         :rtype: Optional[ListNode]
         """
-        carry =0 
-        dummy=ListNode(0)
-        curr=dummy
-        while l1 or l2 or carry:
-            val1 = l1.val if l1 else 0
-            val2 = l2.val if l2 else 0
+        dummy = ListNode()
+        curr = dummy
+        
+        carry=0
+        while l1 or l2 or carry  :
+            v1 = l1.val if l1 else 0
+            v2 = l2.val if l2 else 0
             
-            total = val1 + val2 + carry
-            carry = total // 10
-            new_digit = total % 10
+            # new digit 
+            val = v1 + v2 + carry
+            # 15
+            carry = val // 10
+            val = val % 10
             
-            curr.next = ListNode(new_digit)
+            curr.next = ListNode(val)
+            
+            #update pointers
             curr = curr.next
-            
-            if l1:
-                l1 = l1.next
-            if l2:
-                l2 = l2.next
+            l1=l1.next if l1 else None
+            l2=l2.next if l2 else None
+        
+        # edge case 
+        
         return dummy.next
+
+# time complexity: O(max(m,n)) where m and n are the lengths of the two linked lists.
+# space complexity: O(max(m,n)) for the new linked list.
