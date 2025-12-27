fc31594197b1fc18d95ed91f746e6aed6ef16732
Raghav Mishra <raghav051006@gmail.com>
2025-12-27 13:19:55 +0530

    update 530.py

--- 530.py
+++ 530.py
@@ -3,35 +3,34 @@
 '''
 
 # Definition for a binary tree node.
-# class TreeNode(object):
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
+class TreeNode(object):
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
 class Solution(object):
     def getMinimumDifference(self, root):
         """
         :type root: Optional[TreeNode]
         :rtype: int
         """
-        min_distance = float('inf')
-        prev = [None]
+        self.min_distance = float('inf')
+        self.prev = None
         
         def dfs(node):
-            nonlocal min_distance
             if node is None:
                 return
             
             dfs(node.left)
             
-            if prev[0] is not None:
-                min_distance = min(min_distance, node.val - prev[0])
+            if self.prev is not None:
+                self.min_distance = min(self.min_distance, node.val - self.prev)
             
-            prev[0] = node.val
+            self.prev = node.val
         
             dfs(node.right)
         dfs(root)
-        return min_distance
+        return self.min_distance
     
 # Time Complexity: O(N) where N is the number of nodes in the BST.
 # Space Complexity: O(H) where H is the height of the BST due to the recursion stack.
