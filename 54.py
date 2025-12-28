a0f99e4c36da09c0c9fa811461106ea615b6202c
Raghav Mishra <raghav051006@gmail.com>
2025-12-28 10:46:52 +0530

    Add files via upload

--- 54.py
+++ 54.py
@@ -6,24 +6,36 @@ class Solution(object):
         :type matrix: List[List[int]]
         :rtype: List[int]
         """
-        if not matrix:
-            return []
-        res = []
-        top, bottom, left, right = 0, len(matrix)-1, 0, len(matrix[0])-1
-        while top <= bottom and left <= right:
-            for i in range(left, right+1):
-                res.append(matrix[top][i])
+        output = []
+        
+        left,right = 0, len(matrix[0])
+        top,bottom = 0, len(matrix)
+        
+        while left < right and top < bottom :
+            # get every value in the top row 
+            for i in range (left,right):
+                output.append(matrix [top][i])
             top += 1
-            for i in range(top, bottom+1):
-                res.append(matrix[i][right])
+            
+            # get every value in the right column
+            for i in range (top,bottom):
+                output.append(matrix [i][right-1])
             right -= 1
-            if top <= bottom:
-                for i in range(right, left-1, -1):
-                    res.append(matrix[bottom][i])
-                bottom -= 1
-            if left <= right:
-                for i in range(bottom, top-1, -1):
-                    res.append(matrix[i][left])
-                left += 1
-        return res
-    
+            
+            if not (left < right and top < bottom):
+                break
+            
+            # get every value in the bottom row
+            for i in range (right-1,left-1,-1):
+                output.append(matrix [bottom-1][i])
+            bottom -= 1
+            
+            # get every value in the left column
+            for i in range (bottom-1,top-1,-1):
+                output.append(matrix [i][left])
+            left += 1
+            
+        return output 
+            
+# Time Complexity: O(m*n) where m is the number of rows and n is the number of columns
+# Space Complexity: O(1) ignoring the output list
