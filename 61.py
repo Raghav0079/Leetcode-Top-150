c86f1da42ebf0d150afbd2543bd95435a331815c
Raghav Mishra <raghav051006@gmail.com>
2026-01-01 21:44:17 +0530

    update 61.py

--- 61.py
+++ 61.py
@@ -8,25 +8,25 @@ class Solution(object):
         :rtype: Optional[ListNode]
         """
         if not head:
-            return None
-        # Compute the length of the list and get the tail node
-        length = 1
-        tail = head
+            return head
+        
+        # get length 
+        length,tail = 1, head
         while tail.next:
             tail = tail.next
             length += 1
-        # Make the list circular
-        tail.next = head
-        # Find the new tail: (length - k % length - 1)th node
-        # and the new head: (length - k % length)th node
+            
         k = k % length
-        steps_to_new_head = length - k
-        new_tail = head
-        for _ in range(steps_to_new_head - 1):
-            new_tail = new_tail.next
-        new_head = new_tail.next
-        # Break the circle
-        new_tail.next = None
+        if k == 0:
+            return head
+        
+        # move to pivot and rotate
+        cur = head
+        for i in range(length - k - 1):
+            cur = cur.next
+        new_head=cur.next
+        cur.next=None
+        tail.next=head
         return new_head
-    
-    
+# time complexity: O(n)
+# space complexity: O(1)
