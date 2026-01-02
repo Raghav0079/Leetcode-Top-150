24390a7fdecf96d0140c321363696f0cf09b9e80
Raghav Mishra <raghav051006@gmail.com>
2026-01-02 10:59:06 +0530

    Add files via upload

--- 86.py
+++ 86.py
@@ -9,24 +9,4 @@ class Solution(object):
         :type x: int
         :rtype: Optional[ListNode]
         """
-        before_head = ListNode(0)
-        before = before_head
-        after_head = ListNode(0)
-        after = after_head
-        
-        while head:
-            if head.val < x:
-                before.next = head
-                before = before.next
-            else:
-                after.next = head
-                after = after.next
-            head = head.next
-        after.next = None
-        before.next = after_head.next
-        return before_head.next
-    
-                
-        
-        
         
