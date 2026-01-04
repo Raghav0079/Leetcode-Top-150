6f11031eba7226975d1830e1c838e64620c14586
Raghav Mishra <raghav051006@gmail.com>
2026-01-04 20:17:35 +0530

    update 200.py

--- 200.py
+++ 200.py
@@ -1,3 +1,9 @@
+'''
+Given an m x n 2D binary grid grid which represents a map of '1's (land) and '0's (water), return the number of islands.
+
+An island is surrounded by water and is formed by connecting adjacent lands horizontally or vertically. You may assume all four edges of the grid are all surrounded by water.
+
+'''
 
 
 import collections
@@ -38,4 +44,5 @@ class Solution(object):
                     bfs(r,c)
                     islands +=1
         
+
         return islands
