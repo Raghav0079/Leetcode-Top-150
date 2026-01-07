706c67164f989b53e6813bc10be2b6b07573b285
Raghav Mishra <raghav051006@gmail.com>
2026-01-07 11:25:59 +0530

    update 105.py

--- 105.py
+++ 105.py
@@ -16,10 +16,11 @@ class Solution(object):
         if not preorder or not inorder:
             return None
         
-        root=TreeNode(preorder[0])
-        mid=inorder.index(preorder[0])
-        root.left=self.buildTree(preorder[1:mid+1],inorder[:mid])
-        root.right=self.buildTree(preorder[mid+1:],inorder[mid +1 :])
-        return root 
+        root = TreeNode(preorder[0])
+        mid = inorder.index(preorder[0])
+        root.left = self.buildTree(preorder[1:mid + 1 ],inorder[:mid])
+        root.right = self.buildTree(preorder[mid + 1:],inorder[mid + 1:])
+        return root
+    
     
     
