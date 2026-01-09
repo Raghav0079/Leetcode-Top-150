85f715a8d6fecdc88cd48dfb7ecb38a2e1ec7b02
Raghav Mishra <raghav051006@gmail.com>
2026-01-09 11:46:11 +0530

    update 433.py

--- 433.py
+++ 433.py
@@ -6,23 +6,28 @@ class Solution(object):
         :type bank: List[str]
         :rtype: int
         """
+        # BFS
+        from queue import Queue
         bank  = set(bank)
         if end not in bank:
             return -1
-        
+        # BFS
         queue = Queue()
         queue.put((start, 0))
         visited = {start}
-        
-        while not queue.empty():
+        # BFS
+        while not queue.empty(): # while queue is not empty
             gene,level = queue.get()
+            # Check if we reached the end gene
             if gene == end:
                 return level
+            # Try all possible mutations
             for i in range(len(gene)):
-                for c in 'ACGT':
-                    new_gene =gene  [:i] + letter + gene[i+1:]
+                for c in 'ACGT':  # possible gene characters
+                    new_gene =gene  [:i] + c + gene[i+1:]
                     queue.put((new_gene, level + 1))
                     visited.add(new_gene)
         return -1
-    
+# time complexity: O(N * M) where N is the number of genes in the bank and M is the length of each gene
+# space complexity: O(N)
     
