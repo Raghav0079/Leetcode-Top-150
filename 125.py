405c9d928102970a50c8e76eb95a3be40b97fd27
Raghav Mishra <raghav051006@gmail.com>
2026-01-13 09:37:54 +0530

    update 125.py

--- 125.py
+++ 125.py
@@ -2,13 +2,15 @@
 Alphanumeric characters include letters and numbers.
 Given a string s, return true if it is a palindrome, or false otherwise.'''
 
+
+# intuition : filter out non-alphanumeric characters, convert to lowercase, and check if the string is equal to its reverse.
 class Solution(object):
     def isPalindrome(self,s):
         """
         :type s: str
         :rtype: bool
         """
+        # Filter out non-alphanumeric characters and convert to lowercase
         check = "".join([char.lower() for char in s if char.isalnum()])
-        return check == check[::-1]
-
+        return check == check[::-1]   # Check if the string is equal to its reverse
 
