e37f9377f740e7cc3f456f9b4ca5cb56b8c4c80e
Raghav Mishra <raghav051006@gmail.com>
2026-01-14 11:07:33 +0530

    update 57.py

--- 57.py
+++ 57.py
@@ -39,7 +39,11 @@ class Solution(object):
 # space complexity: O(n)
 # n is the length of intervals
 
-            
+# example usage
+# sol = Solution()
+# print(sol.insert([[1,3],[6,9]],[2,5]))  # Output: [[1,5],[6,9]]
+# print(sol.insert([[1,2],[3,5],[6,7],[8,10],[12,16]],[4,8]))  # Output: [[1,2],[3,10],[12,16]]
+
             
 
             
