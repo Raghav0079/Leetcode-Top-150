18249437c5a1c97a0888876014967ea006778281
Raghav Mishra <raghav051006@gmail.com>
2026-01-14 11:48:41 +0530

    add 452.py

---
+++ 452.py
@@ -0,0 +1,32 @@
+"""
+There are some spherical balloons taped onto a flat wall that represents the XY-plane. The balloons are represented as a 2D integer array points where points[i] = [xstart, xend] denotes a balloon whose horizontal diameter stretches between xstart and xend. You do not know the exact y-coordinates of the balloons.
+
+Arrows can be shot up directly vertically (in the positive y-direction) from different points along the x-axis. A balloon with xstart and xend is burst by an arrow shot at x if xstart <= x <= xend. There is no limit to the number of arrows that can be shot. A shot arrow keeps traveling up infinitely, bursting any balloons in its path.
+
+Given the array points, return the minimum number of arrows that must be shot to burst all balloons.
+"""
+
+class Solution(object):
+    def findMinArrowShots(self, points):
+        """
+        :type points: List[List[int]]
+        :rtype: int
+        """
+        # Sort the balloons based on their starting x-coordinate
+        points.sort()
+        
+        result = len(points)
+        # Initialize the previous balloon to the first one
+        prev = points[0]
+        for i in range(1, len(points)): # Iterate through the balloons starting from the second one
+            curr = points[i]
+            # Check if the current balloon overlaps with the previous one
+            if curr[0] <= prev[1]:
+                result -= 1 
+                prev = [curr[0], min (prev[1], curr[1])]
+            else:
+                prev = curr 
+                
+        return result
+    
+    
