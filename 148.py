24f7feea377d52c96245208cbdf9ee7dd5c89527
Raghav Mishra <raghav051006@gmail.com>
2026-01-17 13:36:16 +0530

    update 148.py

--- 148.py
+++ 148.py
@@ -1,13 +1,14 @@
 """Given the head of a linked list,
 return the list after sorting it in ascending order."""
 
-
+# define solution class
 class Solution(object):
     def sortList(self, head):
         """
         :type head: Optional[ListNode]
         :rtype: Optional[ListNode]
         """
+        # base case
         if not head or not head.next:
             return head
 
@@ -22,14 +23,14 @@ class Solution(object):
         right = self.sortList(right)
 
         return self.merge(left, right)
-
+# helper function to get the middle of the linked list
     def getMid(self, head):
         slow, fast = head, head.next
         while fast and fast.next:
             slow = slow.next
             fast = fast.next.next
         return slow
-
+# helper function to merge two sorted linked lists
     def merge(self, left, right):
         dummy = ListNode(0)
         tail = dummy
