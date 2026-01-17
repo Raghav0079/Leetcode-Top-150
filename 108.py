5d77643f6cfbdd2555f6ea6d49556182b7416140
Raghav Mishra <raghav051006@gmail.com>
2026-01-17 13:18:31 +0530

    update 108.py

--- 108.py
+++ 108.py
@@ -12,12 +12,19 @@ class Solution(object):
         :type nums: List[int]
         :rtype: Optional[TreeNode]
         """
-        if not nums:
-            return None
-        mid = len(nums) // 2
-        root = TreeNode(nums[mid])
-        root.left = self.sortedArrayToBST(nums[:mid])
-        root.right = self.sortedArrayToBST(nums[mid+1:])
-        return root
-    
+        def helper(l,r):
+            if l > r:
+                return None 
         
+            mid = (l+r)//2
+            
+            root = TreeNode(nums[mid])
+            root.left = helper(l,mid-1)
+            root.right = helper(mid+1,r)
+            return root
+        
+        return helper(0,len(nums)-1)
+    
+    
+
+
