0018bca6fb7a11065dc3922a66b60efb2f3a044b
Raghav Mishra <raghav051006@gmail.com>
2026-01-18 11:56:35 +0530

    update 427.py

--- 427.py
+++ 427.py
@@ -1,1 +1,51 @@
+"""
+You are given an array of k linked-lists lists, each linked-list is sorted in ascending order.
+
+Merge all the linked-lists into one sorted linked-list and return it.
+"""
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution(object):
+    def mergeKLists(self, lists):
+        """
+        :type lists: List[Optional[ListNode]]
+        :rtype: Optional[ListNode]
+        """
+        if not lists or len(lists) == 0:
+            return None
+        
+        while len(lists) > 1:
+            mergedList =[]
+            
+            for i in range(0,len(lists),2):
+                l1 = lists[i]
+                l2 = lists [i+1] if (i+1) < len(lists) else None 
+                mergedList.append(self.mergeList(l1,l2))
+            lists = mergedList
+        return lists[0]
+    
+        
+    def mergeList(self,L1,L2):
+        dummy=ListNode(0)
+        tail = dummy
+        
+        while L1 and L2 :
+            if L1.val < L2.val :
+                tail.next = L1
+                L1 = L1.next
+            else :
+                tail.next = L2 
+                L2 = L2.next
+            tail = tail.next
+        if L1 :
+            tail.next = L1
+        if L2 : 
+            tail.next = L2
+        return dummy.next
+                
+        
+
 
