b2571af1400b5b79d6a9f040164a68adadf6a7de
Raghav Mishra <raghav051006@gmail.com>
2026-01-20 09:32:43 +0530

    update 530.py

--- 530.py
+++ 530.py
@@ -1,38 +1,31 @@
-'''Given the root of a Binary Search Tree (BST), return the minimum absolute difference between the values of any two different nodes in the tree.
-
-'''
-
-# Definition for a binary tree node.
 class TreeNode(object):
     def __init__(self, val=0, left=None, right=None):
         self.val = val
         self.left = left
         self.right = right
+
+
 class Solution(object):
     def getMinimumDifference(self, root):
         """
         :type root: Optional[TreeNode]
         :rtype: int
         """
-        min_diff = float('inf')
-        prev = None
-        
+        min_dist = [float("inf")]
+        prev = [None]
+
         def dfs(node):
-            nonlocal min_diff, prev
-            if node is None:
+            if not node:
                 return
-            
+
             dfs(node.left)
-            
-            if prev is not None:
-                min_diff = min(min_diff, node.val - prev)
-            
-            prev = node.val
+
+            if prev[0] is not None:
+                min_dist[0] = min(min_dist[0], abs(node.val - prev[0]))
+
+            prev[0] = node.val
+
             dfs(node.right)
-            
+
         dfs(root)
-        return min_diff
-    
-    
-# Time Complexity: O(N) where N is the number of nodes in the BST.
-# Space Complexity: O(H) where H is the height of the BST due to the recursion stack.
+        return min_dist[0]
