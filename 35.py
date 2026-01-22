42a200cc54cf8f1d465fd546de130f878d3f8329
Raghav Mishra <raghav051006@gmail.com>
2026-01-22 09:11:14 +0530

    update 35.py

--- 35.py
+++ 35.py
@@ -19,3 +19,9 @@ class Solution(object):
             else :
                 right = mid -1
         return left
+
+# Example usage:
+# sol = Solution()
+# print(sol.searchInsert([1,3,5,6], 5))  # Output: 2
+# print(sol.searchInsert([1,3,5,6], 2))  # Output: 1
+# print(sol.searchInsert([1,3,5,6], 7))  # Output: 4
