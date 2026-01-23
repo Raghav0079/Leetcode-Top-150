9d27aaf9671d3a08469e89bf7e6162ddcc73ed9b
Raghav Mishra <raghav051006@gmail.com>
2026-01-23 11:23:29 +0530

    Add files via upload

---
+++ 33.py
@@ -0,0 +1,46 @@
+''''
+There is an integer array nums sorted in ascending order (with distinct values).
+
+Prior to being passed to your function, nums is possibly left rotated at an unknown index k (1 <= k < nums.length) such that the resulting array is [nums[k], nums[k+1], ..., nums[n-1], nums[0], nums[1], ..., nums[k-1]] (0-indexed). For example, [0,1,2,4,5,6,7] might be left rotated by 3 indices and become [4,5,6,7,0,1,2].
+
+Given the array nums after the possible rotation and an integer target, return the index of target if it is in nums, or -1 if it is not in nums.
+
+You must write an algorithm with O(log n) runtime complexity.
+'''
+
+
+class Solution(object):
+    def search(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        l ,r = 0, len(nums) - 1
+        
+        while l <= r:
+            m = (l+r) //2
+            
+            if nums[m] == target:
+                return m 
+            
+            # left sorted portion
+            if nums[l] <= nums[m]:
+                if target > nums[m] :
+                    l = m + 1
+                elif target < nums[l]:
+                    l = m + 1
+                else:
+                    r= m -1 
+                    
+            # right sorted portion
+            else:
+                if target < nums[m]:
+                    r= m - 1
+                elif target > nums[r]:
+                    r= m -1 
+                    
+                else:
+                    l = m +1
+                    
+        return -1
