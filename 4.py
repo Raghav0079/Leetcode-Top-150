d22b1a66429374a45cf5bb98335a62a15a528824
Raghav Mishra <raghav051006@gmail.com>
2026-01-24 10:47:31 +0530

    update 4.py

--- 4.py
+++ 4.py
@@ -12,10 +12,11 @@ class Solution(object):
         :type nums2: List[int]
         :rtype: float
         """
+        # Merging the two sorted arrays
         merged = []
         i,j = 0,0
         m,n = len(nums1),len(nums2)
-        
+        # Merging process
         while i < m and j < n:
             if nums1[i] < nums2[j]:
                 merged.append(nums1[i])
@@ -23,18 +24,18 @@ class Solution(object):
             else:
                 merged.append(nums2[j])
                 j += 1
-                
+                # Append remaining elements
         while i < m:
             merged.append(nums1[i])
             i += 1
-            
+            # Append remaining elements
         while j < n:
             merged.append(nums2[j])
             j += 1
-            
+            # Calculate median
         total = m + n
         mid = total // 2
-        
+        # Check if total length is even or odd
         if total % 2 == 0:
             return (merged[mid - 1] + merged[mid]) / 2.0
         else:
